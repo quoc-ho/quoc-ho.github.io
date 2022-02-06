224ae3eff48964ec349946a729783e6632dda2fb
Quoc Ho <phuquocvn@gmail.com>
2022-02-06 12:25:14 +0800

    Minor interface tweaks

--- research/myScript.js
+++ research/myScript.js
@@ -3,4 +3,9 @@ function toggleAbstract(el) {
     if (abstract.style.display == "block")
         abstract.style.display = "none";
     else abstract.style.display = "block";
+
+    for (abs of Array.from(document.getElementsByTagName("abstract"))) {
+        if (abs != abstract)
+            abs.style.display = "none";
+    }
 }
