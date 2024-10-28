0fbf0310d12714e459d9250994b3a9a75682020a
Quoc Ho <phuquocvn@gmail.com>
2024-10-28 13:41:58 +0000

    Minor edit

--- seminars/gen_pdf_researchsem_swiftlatex.js
+++ seminars/gen_pdf_researchsem_swiftlatex.js
@@ -45,12 +45,12 @@ function makeDocument(info) {
 \end{tikzpicture}
 
 \begin{minipage}{0.07\textwidth}
-  \vspace{-12.5em}\hspace{-3.5em}
+  \vspace{-12.5em}\hspace{-3.8em}
   \scalebox{0.11}{\logo}
 \end{minipage}\hfill
 \begin{minipage}{0.93\textwidth}
   \vspace{-12.5em}
-  \vspace{1.5em}
+  \vspace{1.2em}
   \scalebox{1.35}{\uppercase{\textbf{\textsf{Algebra and Geometry Seminar}}}}
 
   \textsf{The Hong Kong University of Science and Technology}
